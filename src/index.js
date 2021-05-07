e038eb8dedb3d0ee4ac7f80b8a8a9efd0283b993
anaismveras <78924263+anaismveras@users.noreply.github.com>
2021-05-07 14:58:34 -0400

    Added current information to JS
    -Added current weather description
    -Added current City

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 function displayTemp(response) {
-  console.log(response.data.main.temp);
-  let currentTemp = document.querySelector("#currentTemp");
-  currentTemp.innerHTML = Math.round(response.data.main.temp);
+  console.log(response.data);
+  let currentTempElement = document.querySelector("#currentTemp");
+  currentTempElement.innerHTML = Math.round(response.data.main.temp);
+
+  let currentCityElement = document.querySelector("#currentCity");
+  currentCityElement.innerHTML = response.data.name;
+
+  let weatherDescriptionElement = document.querySelector("#weatherDescription");
+  
+  weatherDescriptionElement.innerHTML = response.data.weather[0].description;
 }
 
 let apiKey = "69b40f8beeff9db6adde6eb2258de3ef";
