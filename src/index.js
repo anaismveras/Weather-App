03a5df2f8696bd2267e16cfa25926acf81471439
anaismveras <78924263+anaismveras@users.noreply.github.com>
2021-05-07 15:06:28 -0400

    Added Humidity and Wind Speed to JS file

--- src/index.js
+++ src/index.js
@@ -7,13 +7,18 @@ function displayTemp(response) {
   currentCityElement.innerHTML = response.data.name;
 
   let weatherDescriptionElement = document.querySelector("#weatherDescription");
-  
+
   weatherDescriptionElement.innerHTML = response.data.weather[0].description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "69b40f8beeff9db6adde6eb2258de3ef";
 
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=imperial`;
 
-console.log(apiUrl);
 axios.get(apiUrl).then(displayTemp);
