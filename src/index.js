4c36cddad71097d7d45ef616ef1e06d7e51e3d75
anaismveras <78924263+anaismveras@users.noreply.github.com>
2021-05-07 14:46:47 -0400

    Added information to JS file
    -Added Api
    -Added updated temperature for current city

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function displayTemp(response) {
+  console.log(response.data.main.temp);
+  let currentTemp = document.querySelector("#currentTemp");
+  currentTemp.innerHTML = Math.round(response.data.main.temp);
+}
 
+let apiKey = "69b40f8beeff9db6adde6eb2258de3ef";
+
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=imperial`;
+
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemp);
